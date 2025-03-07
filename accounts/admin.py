a912321ffb00a36126378055419fd5d6bb6d8542
Alok Shandilya <alok.shandilya10@gmail.com>
2025-03-07 18:35:44 +0530

    feat: register user model in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ("email", "first_name", "last_name", "is_active", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("is_active", "is_staff")
